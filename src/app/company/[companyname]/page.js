b883e4ceefeac0260ecc41c20f115caeefeea68f
Patel Shubham <87634631+N3xus97@users.noreply.github.com>
2024-10-23 18:04:12 +0530

    learning example

--- src/app/company/[companyname]/page.js
+++ src/app/company/[companyname]/page.js
@@ -72,7 +72,7 @@ export default function Page({ params }) {
       } else {
         console.log("No facilities data found");
       }
-
+      console.log("hello from shubham")
       setLoading(false);
     };
 
