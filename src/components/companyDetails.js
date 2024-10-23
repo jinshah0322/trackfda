c8668df8f286ae9b6e895b1fa8840c47a63fc8c9
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2024-10-23 14:52:21 +0530

    displayed dynamically in warning letter page

--- src/components/companyDetails.js
+++ src/components/companyDetails.js
@@ -93,10 +93,74 @@ export function Form483sTab({ data }) {
 }
 
 
-export function WarningLettersTab() {
+export function WarningLettersTab({data}) {
   return (
     <>
-      <h1>Warning Letters</h1>
+      {data.length === 0 ? (
+        <div style={{ textAlign: "center", padding: "16px" }}>
+          <h2>No Warning letter available for this company.</h2>
+        </div>
+      ) : (
+        <table style={{ width: "100%", borderCollapse: "collapse" }}>
+          <thead>
+            <tr>
+              <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                Issue Date
+              </th>
+              <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                Company Name
+              </th>
+              <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                FEI Number
+              </th>
+              <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                Issuing Office
+              </th>
+              <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                Subject
+              </th>
+              <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+                
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  {item.letterissuedate}
+                </td>
+                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  {item.legal_name}
+                </td>
+                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  {item.fei_number}
+                </td>
+                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  {item.issuingoffice}
+                </td>
+                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  {item.fei_number}
+                </td>
+                <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                <button style={{
+                      padding: "8px 16px",
+                      backgroundColor: "#007bff",
+                      color: "white",
+                      border: "none",
+                      borderRadius: "4px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => window.open(item.warningletterurl, "_blank")}
+                  >
+                    View
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 }
