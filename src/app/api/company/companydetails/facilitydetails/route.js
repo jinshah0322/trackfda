26766b5066c4e7ca5bd11cb3f51b86f87fc43045
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2024-10-30 14:57:01 +0530

    code restrucuring

--- src/app/api/company/companydetails/facilitydetails/route.js
+++ src/app/api/company/companydetails/facilitydetails/route.js
@@ -9,7 +9,17 @@ export async function GET(req) {
       `SELECT legal_name,fei_number,firm_profile,firm_address from company_details where fei_number=$1`,
       [fei_number]
     );
-    return NextResponse.json({ facilityDetails }, { status: 200 });
+
+    const { rows: inspectionResult } = await query(
+      `
+        SELECT cd.legal_name,cd.fei_number,i.project_area,i.product_type,i.classification,
+        i.posted_citations,i.fiscal_year,i.inspection_end_date FROM company_details cd
+        INNER JOIN inspection_details i ON cd.fei_number = i.fei_number
+        WHERE cd.fei_number = $1
+      `,
+      [fei_number]
+    );
+    return NextResponse.json({ facilityDetails,inspectionResult }, { status: 200 });
   } catch (error) {
     console.error("Error fetching data:", error);
     return NextResponse.json(
