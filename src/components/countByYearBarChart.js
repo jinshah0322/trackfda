d154181247502044953e448ab1a207b5a0d4527c
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2024-11-15 14:26:45 +0530

    created the over view page for investigator details page

---
+++ src/components/countByYearBarChart.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function BarChart({ data }) {
+  const chartData = {
+    labels: data.map((item) => item.year), // Years as labels
+    datasets: [
+      {
+        label: "Investigations",
+        data: data.map((item) => item.investigations), // Investigations count
+        backgroundColor: "rgba(54, 162, 235)",
+        hoverBackgroundColor: "rgba(54, 162, 235,0.7)",
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Form 483s Issued by Year",
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={options} />;
+}
---
+++ src/components/investigatorDetails/overview.js
@@ -0,0 +1,76 @@
+import BarChart from "../countByYearBarChart";
+import Link from "next/link";
+
+export default function Overview({ data }) {
+  const { lastIssuedDate, num483sIssued, overview } = data;
+
+  return (
+    <>
+      <div
+        className="cards-container"
+        style={{ gap: "10px", justifyContent: "flex-start" }}
+      >
+        <div className="card">
+          <p className="card-title">Last Form 483 Issued Date</p>
+          <p className="card-number">
+            {new Date(lastIssuedDate).toLocaleDateString("en-GB", {
+              day: "2-digit",
+              month: "short",
+              year: "numeric",
+            })}
+          </p>
+        </div>
+        <div className="card">
+          <p className="card-title">Form 483s Issued</p>
+          <p className="card-number">{num483sIssued}</p>
+        </div>
+      </div>
+      <div
+        className="chart-container"
+        style={{
+          marginTop: "20px",
+          width: "100%",
+          maxWidth: "800px",
+          height: "400px",
+        }}
+      >
+        {overview && overview.investigationByYear ? (
+          <BarChart data={overview.investigationByYear} />
+        ) : (
+          <h1>No data available for investigations by year.</h1>
+        )}
+      </div>
+
+      <table style={{ width: "100%", borderCollapse: "collapse", marginTop:"20px"}}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Facility Name
+            </th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Facility Location
+            </th>
+            <th style={{ border: "1px solid #ddd", padding: "8px" }}>
+              Issue Dates
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {overview.facilityDetails_issueDate.map((item, index) => (
+            <tr key={index}>
+              <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                  <Link style={{color:"blue",textDecoration:'none'}} href={`/company/${item.legal_name}/facility/${item.fei_number}`}>{item.legal_name}</Link>
+              </td>
+              <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                {item.firm_address}
+              </td>
+              <td style={{ border: "1px solid #ddd", padding: "8px" }}>
+                {item.record_dates}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
