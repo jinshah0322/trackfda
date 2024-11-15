c8492e087fbd5642a3bad7e2416223b0b98b42db
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2024-11-15 18:09:26 +0530

    removed '`'

--- src/app/investigators/investigator_details/page.js
+++ src/app/investigators/investigator_details/page.js
@@ -93,7 +93,7 @@ export default function Page() {
           className={`tab ${activeTab === "form483s" ? "active-tab" : ""}`}
           onClick={() => setActiveTab("form483s")}
         >
-          Form 483`&apos;`s
+          Form 483&apos;s
         </a>
       </div>
 
